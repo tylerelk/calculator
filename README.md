4bbdf1332fa3ccdf9b8fbe2e36e618d5a13dd265
tylerelk <144300250+tylerelk@users.noreply.github.com>
2023-11-04 16:05:06 -0700

    create calculator, add basic operator functions

---
+++ README.md
@@ -0,0 +1,25 @@
+TO DO:
+
+HTML:
+DONE 1. Build calculator body
+DONE 2. Build calculator screen
+DONE 3. Build calculator buttons with unique IDs
+
+CSS:
+DONE 1. Position body in center of viewport
+2. position screen at top of body
+3. position buttons logically
+    3a. operators on RIGHT
+    3b. equal sign at BOTTM RIGHT
+    3c. numbers from CENTER to LEFT in a grid
+    3d. 0 key at bottom (larger?)
+
+JAVA:
+DONE 1. Pull elements from index.html
+DONE 2. create all operator functions
+    2a. Addition
+    2b. Subtraction
+    2c. Multiplication
+    2d. Division
+3. create event listeners
+4. create equation-building function
---
+++ script.js
@@ -0,0 +1,31 @@
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+const operate = document.querySelector('#operate');
+const screen = document.querySelector('#calculation');
+
+function funcAdd(inputA, inputB) {
+    return inputA + inputB;
+}
+
+function funcSub(inputA, inputB) {
+    return inputA - inputB;
+}
+
+function funcMultiply(inputA, inputB) {
+    return inputA * inputB;
+}
+
+function funcDivide(inputA, inputB) {
+    return inputA / inputB;
+}
